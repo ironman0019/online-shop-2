338a62d6bc47149bc4fc6627890daadf6ab8d92b
Emad Isavi <emadesavi@gmail.com>
2025-07-06 02:17:55 +0330

    Create admin panel layouts blade files

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fa">
+
+<head>
+    @include('admin.layouts.head-tag')
+</head>
+
+<body dir="rtl">
+
+
+    @include('admin.layouts.partials.header')
+
+    <section class="body-container">
+
+        @include('admin.layouts.partials.sidebar')
+
+        <section id="main-body" class="main-body">
+
+            @yield('content')
+
+        </section>
+
+    </section>
+
+
+    @include('admin.layouts.scripts')
+</body>
+
+</html>
