56395c1b1c63aa54a2f6205d1e1d921f1dad976a
Emad Isavi <emadesavi@gmail.com>
2025-07-06 02:20:06 +0330

    Add admin route groups and add admin dashboard page route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::prefix('admin')->middleware([])->name('admin.')->group(function() {
+
+    Route::get('/', [AdminDashboardController::class, 'index'])->name('dashboard');
+
+
 });
