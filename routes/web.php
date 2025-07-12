88a4d946640ec6c9bcc54a49e9fe361f8444f65e
Emad Isavi <emadesavi@gmail.com>
2025-07-13 03:24:10 +0330

    Add admin panel market category, brand and comment resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\Market\BrandController;
+use App\Http\Controllers\Admin\Market\CategoryController;
+use App\Http\Controllers\Admin\Market\CommentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +15,15 @@ Route::get('/', function () {
 Route::prefix('admin')->middleware([])->name('admin.')->group(function() {
 
     Route::get('/', [AdminDashboardController::class, 'index'])->name('dashboard');
+
+    // Market routes
+    Route::prefix('market')->name('market.')->group(function() {
+        
+        Route::resource('category', CategoryController::class);
+        Route::resource('brand', BrandController::class);
+        Route::resource('comment', CommentController::class);
+
+    });
 
 
 });
