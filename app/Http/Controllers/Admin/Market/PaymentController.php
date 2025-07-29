b7e64ab53752cae39430728821c51fc4a8d8b058
Emad Isavi <emadesavi@gmail.com>
2025-07-29 13:18:08 +0330

    Create admin panel market payment controller and index page blade file

---
+++ app/Http/Controllers/Admin/Market/PaymentController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Market;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    public function index()
+    {
+        return view('admin.market.payment.index');
+    }
+
+    public function online()
+    {
+        //
+    }
+
+    public function offline()
+    {
+        //
+    }
+
+    public function attendence()
+    {
+        //
+    }
+
+    public function confirm()
+    {
+        //
+    }
+}
