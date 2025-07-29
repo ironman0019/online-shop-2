5a70a57dc7ef8e74d96dbc2fd9550815c30c98a2
Emad Isavi <emadesavi@gmail.com>
2025-07-29 13:17:11 +0330

    Create admin panel market order controller and index page blade file

---
+++ app/Http/Controllers/Admin/Market/OrderController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Market;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function all()
+    {
+        return view('admin.market.order.index');
+    }
+
+    public function show()
+    {
+        //
+    }
+
+    public function newOrders()
+    {
+        //
+    }
+
+    public function sending()
+    {
+        //
+    }
+
+    public function unpaid()
+    {
+        //
+    }
+
+    public function canceled()
+    {
+        //
+    }
+
+    public function returned()
+    {
+        //
+    }
+
+    public function changeSendStatus()
+    {
+        //
+    }
+
+    public function changeOrderStatus()
+    {
+        //
+    }
+
+    public function cancelOrder()
+    {
+        //
+    }
+
+
+}
