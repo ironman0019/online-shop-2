e8b4333c2f27a13873d7f83f919024580f62f384
Emad Isavi <emadesavi@gmail.com>
2025-07-29 13:19:43 +0330

    Create admin panel market gallery controller

---
+++ app/Http/Controllers/Admin/Market/GalleryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Market;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GalleryController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+
+    
+}
