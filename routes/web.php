ce944c09bf60fd563bec9fc649fbb94de603939f
Emad Isavi <emadesavi@gmail.com>
2025-08-12 00:28:08 +0330

    Install jetstream package with Livewire

--- routes/web.php
+++ routes/web.php
@@ -148,3 +148,13 @@ Route::prefix('admin')->middleware([])->name('admin.')->group(function() {
 
 
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
