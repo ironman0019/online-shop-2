123460b48ad217de8447c3d13b2eeb24e301a902
Emad Isavi <emadesavi@gmail.com>
2025-10-23 02:04:25 +0330

    Add sluggable package to project

--- app/Models/Content/PostCategory.php
+++ app/Models/Content/PostCategory.php
@@ -2,12 +2,29 @@
 
 namespace App\Models\Content;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PostCategory extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, Sluggable;
 
     protected $guarded = ['id'];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    
 }
