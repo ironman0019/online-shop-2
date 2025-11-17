9c10c329786e10b685339b6a17b0f1c97afd2076
Emad Isavi <emadesavi@gmail.com>
2025-11-18 02:47:05 +0330

    Implement the CRUD functions in admin panel content post controller

--- app/Http/Controllers/Admin/Content/PostController.php
+++ app/Http/Controllers/Admin/Content/PostController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Admin\Content;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Content\PostRequest;
+use App\Http\Services\ImageService\ImageService;
+use App\Models\Content\Post;
+use App\Models\Content\PostCategory;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -12,7 +16,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('admin.content.post.index');
+        $posts = Post::orderBy('created_at', 'desc')->paginate('10');
+        return view('admin.content.post.index', compact('posts'));
     }
 
     /**
@@ -20,15 +25,36 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view('admin.content.post.create');
+        $categories = PostCategory::all();
+        return view('admin.content.post.create', compact('categories'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PostRequest $request, ImageService $imageService)
     {
-        //
+        $inputs = $request->all();
+
+        if($request->hasFile('image')) {
+            $imageService->setExclusiveDirectory('images' . DIRECTORY_SEPARATOR . 'post');
+            $result = $imageService->createIndexAndSave($request->file('image'));
+
+            if($result === false) {
+                return back()->with('swal-error', 'خطا در آپلود عکس');
+            }
+            $inputs['image'] = $result;
+        }
+
+        //date fixed
+        $realTimestampStart = substr($request->published_at, 0, 10);
+        $inputs['published_at'] = date("Y-m-d H:i:s", (int)$realTimestampStart);
+
+        $inputs['author_id'] = auth()->user()->id ?? 1;
+        
+        Post::create($inputs);
+        
+        return to_route('admin.content.post.index')->with('swal-success', 'پست با موفقیت ایجاد شد');
     }
 
     /**
@@ -42,24 +68,85 @@ class PostController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Post $post)
     {
-        //
+        $categories = PostCategory::all();
+        return view('admin.content.post.edit', compact('categories', 'post'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(PostRequest $request, Post $post, ImageService $imageService)
     {
-        //
+        $inputs = $request->all();
+
+        if ($request->hasFile('image')) {
+            // Remove old image
+            $imageService->deleteIndex($post->image);
+
+            $imageService->setExclusiveDirectory('images' . DIRECTORY_SEPARATOR . 'post');
+            $result = $imageService->createIndexAndSave($request->file('image'));
+
+            if ($result === false) {
+                return back()->with('swal-error', 'خطا در آپلود عکس');
+            }
+            $inputs['image'] = $result;
+        }
+        else
+        {
+            if(isset($inputs['currentImage']) && !empty($post->image)) {
+                $image = $post->image;
+                $image['currentImage'] = $inputs['currentImage'];
+                $inputs['image'] = $image;
+            }
+        }
+
+        //date fixed
+        $realTimestampStart = substr($request->published_at, 0, 10);
+        $inputs['published_at'] = date("Y-m-d H:i:s", (int)$realTimestampStart);
+
+        $post->update($inputs);
+        return to_route('admin.content.post.index')->with('swal-success', 'پست با موفقیت آپدیت شد');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return to_route('admin.content.post.index')->with('swal-success', 'پست با موفقیت حذف شد');
     }
+
+    public function status(Post $post)
+    {
+        $post->status = $post->status == 0 ? 1 : 0;
+        $result = $post->save();
+        if ($result) {
+            if ($post->status == 0) {
+                return response()->json(['status' => true, 'checked' => false]);
+            } else {
+                return response()->json(['status' => true, 'checked' => true]);
+            }
+        } else {
+            return response()->json(['status' => false]);
+        }
+    }
+
+    public function commentable(Post $post)
+    {
+        $post->commentable = $post->commentable == 0 ? 1 : 0;
+        $result = $post->save();
+        if ($result) {
+            if ($post->commentable == 0) {
+                return response()->json(['status' => true, 'checked' => false]);
+            } else {
+                return response()->json(['status' => true, 'checked' => true]);
+            }
+        } else {
+            return response()->json(['status' => false]);
+        }
+    }
+
 }
